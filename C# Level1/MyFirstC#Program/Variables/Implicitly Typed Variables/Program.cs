360773806fcc826e08da2445ad03823677dc719d
Ibrahim Imad <ibrahim770476@gmail.com>
2025-08-17 17:10:56 +0300

    Add files via upload

---
+++ C# Level1/MyFirstC#Program/Variables/Implicitly Typed Variables/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 10;
+            var y = 10.5;
+            var z = "Ibrahim";
+
+            Console.WriteLine("{0} {1} {2}", x, y, z);
+        }
+    }
+}
---
+++ C# Level1/MyFirstC#Program/Variables/Variables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Ibrahim Imad";
+
+            Console.WriteLine(MyName);
+
+            int x = 20, y = 100;
+
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("y = " + y);
+
+            Console.WriteLine("x + y = " + (x + y));
+
+            double MyDouble = 25.33;
+            char MyLetter = 'I';
+            bool MyBool = true;
+        }
+    }
+}
